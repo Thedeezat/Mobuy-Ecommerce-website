5f74ef3f432d34e6b6d6d30b2b4432efdc7b4c77
Hadiza <Hadizavidime@gmail.com>
2023-01-01 17:59:20 +0300

    env setup done

--- src/components/Auth/Firebase.js
+++ src/components/Auth/Firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC2ccHn0o2vf2IoHyzIUx3bKBucYkgJj4I',
-  authDomain: 'mobuy-web-app.firebaseapp.com',
-  projectId: 'mobuy-web-app',
-  storageBucket: 'mobuy-web-app.appspot.com',
-  messagingSenderId: '704377276206',
-  appId: '1:704377276206:web:1ea3b76bcec6ae46d9dcbf',
-  measurementId: 'G-LD54XYH9JZ',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig)
